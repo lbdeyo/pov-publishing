e3c875d4c6bb5c737fad9c81d28c52a644bbf784
L.B. Deyo <lbdeyo@gmail.com>
2024-11-24 12:15:40 -0600

    Filling out the history podcast page

--- src/app/history-podcast/page.tsx
+++ src/app/history-podcast/page.tsx
@@ -3,27 +3,128 @@ export default function HistoryPodcast() {
     <div className="container mx-auto px-4 md:px-16 pt-4 md:pt-8 max-w-6xl">
       <div className="grid grid-cols-1 gap-y-8 md:gap-y-16">
         <div>
-          <h1>All Our Yesterdays Podcast</h1>
+          <h1>All Our Yesterdays: A history podcast</h1>
           <p>Explore fascinating historical narratives and discussions.</p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 gap-y-16">
           <div>
             <iframe
-              title="1. The Big Bang"
+              title="8. Early Ancient China"
               height="150"
               width="100%"
-              style={{
-                border: "none",
-                minWidth: "min(100%, 430px)",
-                height: "150px",
-              }}
               data-name="pb-iframe-player"
-              src="https://www.podbean.com/player-v2/?from=embed&i=6kz97-161836a-pb&share=1&download=1&fonts=Arial&skin=1&font-color=&rtl=0&logo_link=&btn-skin=7&size=150"
-              loading="lazy"
-            />
-            <h2 className="mt-3 mb-1">1. The Big Bang</h2>
-            <p>And away we go, with the beginning of everything.</p>
+              src="https://www.podbean.com/player-v2/?i=rejri-1742e67-pb&from=pb6admin&share=1&download=1&rtl=0&fonts=Arial&skin=1&font-color=auto&logo_link=episode_page&btn-skin=7"
+              loading="lazy"></iframe>
+            <h2 className="mt-5 mb-1">8. Early Ancient China ​</h2>
+            <p>
+              A new civilization rises in the East, bringing sophisticated
+              organization, profound philosophy, and bodies buried in city
+              walls. 
+            </p>
+          </div>
+          <div>
+            <iframe
+              title="7. Encrypted History: The Indus Valley and Minoan Civilizations"
+              height="150"
+              width="100%"
+              data-name="pb-iframe-player"
+              src="https://www.podbean.com/player-v2/?i=d6i4m-1705e26-pb&from=pb6admin&share=1&download=1&rtl=0&fonts=Arial&skin=1&font-color=auto&logo_link=episode_page&btn-skin=7"
+              loading="lazy"></iframe>
+            <h2 className="mt-5 mb-1">7. Encrypted History ​</h2>
+            <p>
+              The Indus Valley Civilization and the Minoan Civilization were
+              both highly advanced, highly influential nations of the ancient
+              world, yet our knowledge of them is obscured by our inability to
+              decipher their writing systems (Indus Valley Script and Linear
+              A). 
+            </p>
+          </div>
+          <div>
+            <iframe
+              title="6. Egypt in the Age of the Pyramids"
+              height="150"
+              width="100%"
+              data-name="pb-iframe-player"
+              src="https://www.podbean.com/player-v2/?i=rzkj6-16ca2d5-pb&from=pb6admin&share=1&download=1&rtl=0&fonts=Arial&skin=1&font-color=auto&logo_link=episode_page&btn-skin=7"
+              loading="lazy"></iframe>
+            <h2 className="mt-5 mb-1">6. Egypt in the Age of the Pyramids ​</h2>
+            <p>
+              The splendor of life, death and rebirth in the Old Kingdom of
+              unified Egypt. Life on the banks of the Nile and in the shadow of
+              the pyramids, amid the wealth and power of the early dynasties.
+              Imhotep, the first great genius of history, who started as a
+              common man but became a vizier, a god, and then a monster.
+            </p>
+          </div>
+
+          <div>
+            <iframe
+              title="5. The Dawn of Civilization"
+              height="150"
+              width="100%"
+              scrolling="no"
+              data-name="pb-iframe-player"
+              src="https://www.podbean.com/player-v2/?i=iaaqu-16a31fe-pb&from=pb6admin&share=1&download=1&rtl=0&fonts=Arial&skin=1&font-color=auto&logo_link=episode_page&btn-skin=7"
+              loading="lazy"></iframe>
+            <h2 className="mt-5 mb-1">5. The Dawn of Civilization ​</h2>
+            <p>
+              Cities. Writing. The wheel. The plow. Literature and epic poetry.
+              Irrigation systems. The sailboat. Beer. The concept of time. These
+              are all inventions of the first great civilization, Mesopotamia,
+              and all of them were to be found in the first city of history,
+              Uruk. Join Lance, L.B. and producer William Gold for a discussion
+              of how the story of history began. 
+            </p>
+          </div>
+          <div>
+            <iframe
+              title="3. Life--uh--Finds a Way"
+              height="150"
+              width="100%"
+              data-name="pb-iframe-player"
+              src="https://www.podbean.com/player-v2/?from=embed&i=vczhf-1661c51-pb&share=1&download=1&fonts=Arial&skin=1&font-color=&rtl=0&logo_link=&btn-skin=7&size=150"
+              loading="lazy"></iframe>
+            <h2 className="mt-5 mb-1">3. Life&mdash;uh&mdash;finds a way​</h2>
+            <p>
+              The primordial Earth. The age of planetesimal bombardment. The
+              origins and nature of life. Cosmic rays and plate tectonics,
+              watery asteroids and organic chemicals. The Fermi Paradox, the
+              Drake Equation, and the mystery of our apparently unique existence
+              as living things in the universe. Join L.B. and Lance as they
+              grapple with the seemingly incomprehensible.
+            </p>
+          </div>
+          <div>
+            <iframe
+              title="4. The Prehistory of Humanity"
+              height="150"
+              width="100%"
+              data-name="pb-iframe-player"
+              src="https://www.podbean.com/player-v2/?from=embed&i=t7j45-16824b4-pb&share=1&download=1&fonts=Arial&skin=1&font-color=&rtl=0&logo_link=&btn-skin=7&size=150"
+              loading="lazy"></iframe>
+            <h2 className="mt-5 mb-1">4. The Prehistory of Humanity ​</h2>
+            <p>
+              Was the prehistoric human a rough beast crouching in the forest
+              primeval, capable only of subsistence and superstition? Or were
+              early homo sapiens brave, adventurous, and rich in culture? What
+              distinguishes the human species?
+            </p>
+          </div>
+          <div>
+            <iframe
+              title="2. Lives of a Quark"
+              allowtransparency="true"
+              height="150"
+              width="100%"
+              data-name="pb-iframe-player"
+              src="https://www.podbean.com/player-v2/?from=embed&i=c9hif-164343a-pb&share=1&download=1&fonts=Arial&skin=1&font-color=&rtl=0&logo_link=&btn-skin=7&size=150"
+              loading="lazy"></iframe>
+            <h2 className="mt-5 mb-1">2. Lives of a Quark​</h2>
+            <p>
+              In the second episode, Lance and L.B. follow a single quark
+              through its 9 billion year journey into the expanding universe.
+            </p>
           </div>
           <div>
             <iframe
@@ -39,11 +140,8 @@ export default function HistoryPodcast() {
               src="https://www.podbean.com/player-v2/?from=embed&i=6kz97-161836a-pb&share=1&download=1&fonts=Arial&skin=1&font-color=&rtl=0&logo_link=&btn-skin=7&size=150"
               loading="lazy"
             />
-            <h2 className="mt-3 mb-1">2. Lives of a Quark​</h2>
-            <p>
-              In the second episode, Lance and L.B. follow a single quark
-              through its 9 billion year journey into the expanding universe.
-            </p>
+            <h2 className="mt-5 mb-1">1. The Big Bang</h2>
+            <p>And away we go, with the beginning of everything.</p>
           </div>
         </div>
       </div>
