c74bec264a07df638743dd63c613d1f98d59d8e0
L.B. Deyo <lbdeyo@gmail.com>
2024-11-26 17:26:00 -0600

    Adding favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -49,6 +49,18 @@ export const metadata: Metadata = {
       "Great conversations are one of life's deepest pleasures. At Persistence of Vision Publishing, our mission is to inspire more of these meaningful exchanges by publishing daring and innovative work across media.",
     images: ["/img/pov-og.jpg"],
   },
+  icons: {
+    icon: [
+      {url: "/favicon.ico"},
+      {url: "/favicon-16x16.png", sizes: "16x16", type: "image/png"},
+      {url: "/favicon-32x32.png", sizes: "32x32", type: "image/png"},
+    ],
+    apple: [{url: "/apple-touch-icon.png"}],
+    other: [
+      {rel: "android-chrome-192x192", url: "/android-chrome-192x192.png"},
+      {rel: "android-chrome-512x512", url: "/android-chrome-512x512.png"},
+    ],
+  },
 };
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
