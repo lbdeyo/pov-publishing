6a7ce0a18274c190aab0cf79fb6187a9db9300cc
L.B. Deyo <lbdeyo@gmail.com>
2024-11-26 11:09:55 -0600

    Adding the OG

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import {Zilla_Slab_Highlight, Cormorant_Garamond} from "next/font/google";
 import Navbar from "./components/navbar";
 import "./globals.css";
+import type {Metadata} from "next";
 
 const cormorantGaramond = Cormorant_Garamond({
   subsets: ["latin"],
@@ -13,6 +14,38 @@ const zillaSlabHighlight = Zilla_Slab_Highlight({
   weight: ["400", "700"],
   variable: "--font-zilla",
 });
+
+export const metadata: Metadata = {
+  metadataBase: new URL("https://povpublishing.com"), // Replace with your actual domain
+  title: {
+    default: "Persistence of Vision Publishing",
+    template: "%s | POV Publishing",
+  },
+  description:
+    "Great conversations are one of life's deepest pleasures. At Persistence of Vision Publishing, our mission is to inspire more of these meaningful exchanges by publishing daring and innovative work across media.",
+  openGraph: {
+    type: "website",
+    title: "Persistence of Vision Publishing",
+    description:
+      "Great conversations are one of life's deepest pleasures. At Persistence of Vision Publishing, our mission is to inspire more of these meaningful exchanges by publishing daring and innovative work across media.",
+    images: [
+      {
+        url: "/img/pov-og.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Welcome to Persistence of Vision Publishing",
+      },
+    ],
+    siteName: "POV Publishing",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Persistence of Vision Publishing",
+    description:
+      "Great conversations are one of life's deepest pleasures. At Persistence of Vision Publishing, our mission is to inspire more of these meaningful exchanges by publishing daring and innovative work across media.",
+    images: ["/img/pov-og.jpg"],
+  },
+};
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
   return (
